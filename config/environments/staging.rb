dce4164500144cbce2f040d08e795afc52cabdf1
Adam Burmister <adam.burmister@gmail.com>
2014-08-26 23:16:08 -0700

    Add envs

---
+++ config/environments/staging.rb
@@ -0,0 +1,10 @@
+require_relative "production"
+
+Mail.register_interceptor(
+  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
+)
+
+Rails.application.configure do
+  config.log_level = :debug
+  config.action_mailer.default_url_options = { host: ENV.fetch("SITE_HOST") }
+end
