9045e0967f3196098830ad1510209750d5dda8a9
Adam Burmister <adam.burmister@gmail.com>
2014-09-07 14:56:15 -0700

    initialize_on_precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,3 +8,5 @@ Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( organisation_memberships.js, xero_engine/mailer.css )
+
+Rails.application.config.assets.initialize_on_precompile = false
