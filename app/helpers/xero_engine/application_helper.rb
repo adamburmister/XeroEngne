4672164d9ad7635ff29eb859a067677203e735ff
Adam Burmister <adam.burmister@gmail.com>
2014-12-27 08:27:40 -0800

    head_stylesheet_tag helper

--- app/helpers/xero_engine/application_helper.rb
+++ app/helpers/xero_engine/application_helper.rb
@@ -1,6 +1,8 @@
 module XeroEngine
   module ApplicationHelper
 
+    @@head_stylesheets = []
+
     # def main_app
     #   Rails.application.class.routes.url_helpers
     # end
@@ -11,6 +13,16 @@ module XeroEngine
 
     def site_url
       ENV.fetch('SITE_URL')
+    end
+
+    def head_stylesheet_tag(url)
+      @@head_stylesheets << url
+    end
+
+    def head_stylesheet_tags
+      @@head_stylesheets.map do |url|
+        stylesheet_link_tag url
+      end.join("\n").html_safe
     end
 
   end
